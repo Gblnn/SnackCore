e42ebb50ba4f8e50d4323259311cd47e43f41eba
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-05-07 12:08:32 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home"
+import LandingPage from "./pages/landing-page"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<LandingPage/>}/>
       <Route path="/home" element={<Home/>}/>
     </Routes>
     </BrowserRouter>
---
+++ src/pages/landing-page.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+
+export default function LandingPage(){
+    return(
+        <div style={{height:"100svh", width:"100%", background:"linear-gradient(90deg, purple, mediumslateblue)", display:"flex", justifyContent:"center", alignItems:"center", color:"white", flexFlow:"column"}}>
+            <h3>Welcome to</h3>
+            <div style={{display:"flex", alignItems:"center", justifyContent:'center', border:""}}>
+                <img src="vending.png"/>
+                <h1 style={{fontSize:"3rem"}}>SnackCore</h1>
+            </div>
+            <p style={{fontSize:"0.8rem"}}>Bringing your favorite snacks to your doorstep🎊</p>
+
+            <br/>
+            <br/>
+            <Link to="/home">
+            <button>Get Started</button>
+            </Link>
+            
+        </div>
+    )
+}
