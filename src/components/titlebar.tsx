2d07a40017daa41848c9ec2f7d682dbe86eff3be
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-05-08 14:26:41 +0400

    Update titlebar.tsx

--- src/components/titlebar.tsx
+++ src/components/titlebar.tsx
@@ -9,7 +9,7 @@ export default function TitleBar(props:Props){
 
     
     return(
-        <div id="base-layer" style={{background:"rgba(240 240 240/ 75%)",backdropFilter:"blur(16px)", height:"5rem", display:"flex", alignItems:"center", boxShadow:"1px 1px 20px rgba(0 0 0/ 50%)", position:"fixed", width:"100%", top:0, zIndex:1, borderBottomLeftRadius:"", borderBottomRightRadius:"" }}>
+        <div id="base-layer" style={{background:"rgba(240 240 240/ 100%)",backdropFilter:"blur(16px)", height:"5rem", display:"flex", alignItems:"center", boxShadow:"1px 1px 20px rgba(0 0 0/ 50%)", position:"fixed", width:"100%", top:0, zIndex:1, borderBottomLeftRadius:"", borderBottomRightRadius:"" }}>
 
             <div id="primary-layer" style={{border:"", margin:"1.5rem", width:"100%", display:"flex", justifyContent:"space-between", alignItems:"center"}}>
 
