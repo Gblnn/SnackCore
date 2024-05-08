3acc8b9666362c3946116ebe48f8b3f471b5c46a
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-05-08 14:06:26 +0400

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,11 +8,11 @@ interface Props{
 export default function Navbar(props:Props){
     return(
         <div style={{display:"flex", alignItems:"center", justifyContent:"space-around", height:"4rem", background:"rgb(250 250 250)", boxShadow:"1px 1px 20px rgba(0 0 0/ 50%)", position:"fixed", bottom:0, width:"100%"}} className={props.className}>
-            <Link to="/0e/home" >
+            <Link to="/0e/home" style={{display:"flex"}}>
             <ChefHat className={location.pathname=="/0e/home"?"nav-active":"nav-inactive"}/>
             </Link>
             
-            <Link to="/0e/profile" >
+            <Link to="/0e/profile" style={{display:"flex"}} >
             <User className={location.pathname=="/0e/profile"?"nav-active":"nav-inactive"}/>
             </Link>
             
