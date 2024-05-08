e7ab5c6f8f1e3580775f614eac8843a798ddfbcb
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-05-08 14:15:22 +0400

    update

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -8,7 +8,7 @@ export default function SearchBar(props:Props){
     return(
         <div style={{background:"rgba(250 250 250/ 75%)",backdropFilter:"blur(16px)", height:"3rem", display:"flex", margin:"1.5rem", borderRadius:"1rem", boxShadow:"1px 1px 10px rgba(0 0 0/ 25%)", alignItems:"center", gap:"1rem"}}>
         
-            <SearchIcon style={{border:"", marginLeft:"1rem"}} color="purple"/>
+            <SearchIcon style={{border:"", marginLeft:"1rem"}} color="crimson"/>
             <input placeholder={props.placeholderText}/>
             
         </div>
